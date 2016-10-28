49f7a7bde668cccb5ec468f71daaa3d366ff35c0
Jonathan Heemstra <jonathan.heemstra@gmail.com>
2016-10-28 16:25:13 -0700

    complete class 5 code workshop

--- starter-code/scripts/blogArticles.js
+++ starter-code/scripts/blogArticles.js
@@ -1,4 +1,5 @@
 var ourLocalData = [
+  {"title":"Title","body":"<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>\n<pre><code>function sayHi() {\nconsole.log(&#39;hello&#39;);\n}\n</code></pre>","author":"Jonny","authorUrl":"www","category":"food","publishedOn":"2016-10-28T23:14:04.171Z","daysAgo":0,"publishStatus":"(published today)"},
   {
     title:       'Bacon Ipsum',
     category:    'food',
